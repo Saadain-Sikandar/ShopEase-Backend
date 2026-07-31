a211bbeadf501d4223636bb7f9a118349e2decf3
Saadain-Sikandar <saadain4008@gmail.com>
2026-07-31 18:01:20 +0500

    completed

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import ConnectDB from "./DataBase/db.js";
-import adminRoutes from './Routes/adminRoutes.js';
+import adminRoutes from "./Routes/adminRoutes.js";
 import authRoutes from "./Routes/authRoutes.js";
-import cartRoutes from './Routes/cartRoutes.js';
-import orderRoutes from './Routes/orderRoutes.js';
+import cartRoutes from "./Routes/cartRoutes.js";
+import orderRoutes from "./Routes/orderRoutes.js";
 import productsRoutes from "./Routes/productsRoutes.js";
 import userRoutes from "./Routes/userRoutes.js";
-import wishListRoutes from './Routes/wishListRoutes.js';
+import wishListRoutes from "./Routes/wishListRoutes.js";
+
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
@@ -18,23 +19,23 @@ app.use(express.json());
 ConnectDB();
 
 // routes
-// auth 
+// auth
 app.use("/api/auth", authRoutes);
 // user
 app.use("/api/user", userRoutes);
 // products
 app.use("/api/products", productsRoutes);
-// cart 
-app.use('/api/cart',cartRoutes)
-// wihslist 
-app.use("/api/wishlist",wishListRoutes)
-// orders 
-app.use("/api/orders",orderRoutes)
+// cart
+app.use("/api/cart", cartRoutes);
+// wihslist
+app.use("/api/wishlist", wishListRoutes);
+// orders
+app.use("/api/orders", orderRoutes);
 // Admin
-app.use("/api/admin",adminRoutes)
+app.use("/api/admin", adminRoutes);
 
-// images 
-app.use("/uploads",express.static("uploads"))
+// images
+app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -43,3 +44,4 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is now running on port http://localhost:${PORT}`);
 });
+
